9957cb5a2f6c696522cbdadabfee20007def5e42
NickLee0310 <36125202+NickLee0310@users.noreply.github.com>
2020-02-20 10:14:28 +0800

    Create LRU Cache.cpp

---
+++ leetcode/LRU Cache.cpp
@@ -0,0 +1,64 @@
+/*
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+The cache is initialized with a positive capacity.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+LRUCache cache = new LRUCache( 2 /* capacity */ );
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+*/
+class LRUCache {    
+public:
+    LRUCache(int cap)
+        :capacity(cap)
+    {}
+    int get(int key) 
+    {
+        if(data.find(key) == data.end())
+            return -1;
+        used(key);
+        return data[key];
+    }
+    void put(int key, int value) 
+    {
+        used(key);
+        data[key] = value;
+    }
+private:
+    int capacity;
+    unordered_map<int, int> data;
+    unordered_map<int, list<int>::iterator> pos;
+    list<int> recent;
+    void used(int key)
+    {
+        if(pos.find(key) != pos.end())
+        {
+            recent.erase(pos[key]);
+        }
+        else if(pos.size() == capacity)
+        {
+            int old = recent.back();
+            recent.pop_back();
+            data.erase(old);
+            pos.erase(old);
+        }
+        recent.push_front(key);
+        pos[key] = recent.begin();
+    }
+};
